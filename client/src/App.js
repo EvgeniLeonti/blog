41a8fbd519510a29cb008fe2be2c9237b8f9810f
Jenya <leo@campus.technion.ac.il>
2019-12-08 20:02:28 +0200

    bootstrap 4 blog theme added

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from "graphql-tag";
 
-
 const GET_ALL_POSTS = gql`
-query {
-  allPosts {
-    id title timestamp summary content
-  }
-}
-`
+    query {
+      allPosts {
+        id title timestamp summary content
+      }
+    }
+`;
+
 function App() {
   const { data, loading, error } = useQuery(GET_ALL_POSTS);
 
@@ -21,29 +21,19 @@ function App() {
 
   return (
     <React.Fragment>
-      <h1>
-        Pokémons
-  
-</h1>
-
-      <p>
-        <a href="https://en.wikipedia.org/wiki/List_of_Pok%C3%A9mon">The Pokémon franchise</a> revolves around 832 fictional species of collectible monsters, each having unique designs and skills. Conceived by Satoshi Tajiri in early 1989, Pokémon are creatures that inhabit the fictional Pokémon World.
-        This is the list of the first 150 Pokémon as they appear in Pokémon Stadium, starting with Bulbasaur in the top left corner and ending with Mewtwo in the bottom right corner.
-        
-</p>
-        <div className="container">
+        <div className="col-lg-8 col-md-10 mx-auto">
             {data && data.allPosts && data.allPosts.map((post, index) => (
-                <div key={index} className="card">
-                    <img src={post.image}/>
-                    <div class="card-body">
-                        <h2>{post.title}</h2>
-                        <h3>{post.timestamp}</h3>
-                        <p>
-                            {post.content}
-                        </p>
-                    </div>
+                <div key={index} className="post-preview">
+                    <a href="post.html">
+                        <h2 className="post-title">
+                            {post.title}
+                        </h2>
+                        <h3 className="post-subtitle">
+                            {post.summary}
+                        </h3>
+                    </a>
+                    <p className="post-meta">Posted by <a href="#">Start Bootstrap</a> on {post.timestamp}</p>
                 </div>
-
             ))}
         </div>
     </React.Fragment>
--- client/src/index.js
+++ client/src/index.js
@@ -12,12 +12,12 @@ import { ApolloProvider } from '@apollo/react-hooks';
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: 'http://localhost:4000/graphql'
-})
+});
 
 const client = new ApolloClient({
   cache,
   link
-})
+});
 
 
 ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
