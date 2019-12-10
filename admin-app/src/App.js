ec57228e19e38d142cb23d8c6e18395ef8c41f62
Evgeni Leonti <evgeni.leonti@finanda.co.il>
2019-12-10 16:42:03 +0200

    get post fields hook added

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,12 +1,26 @@
 // reference: https://www.taniarascia.com/crud-app-in-react-with-hooks/
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {useQuery, useMutation} from '@apollo/react-hooks';
 import './App.css';
 import PostTable from './tables/PostTable'
 import AddPostForm from './forms/AddPostForm'
 import gql from "graphql-tag";
 import EditPostForm from "./forms/EditPostForm";
+
+
+const GET_POST_FIELDS = gql`
+    query {
+        __type(name: "Post") {
+            name
+            fields {
+                name
+                type {
+                    name
+                }
+            }
+        }
+    }`;
 
 
 const GET_ALL_POSTS = gql`
@@ -39,19 +53,17 @@ const DELETE_POST = gql`
 `;
 
 function App() {
+    // Declare a new state variable named "editing". state variables are preserved by React.
+    // useState returns a pair of values: the current state and a function that updates it
+
+    // custom hooks
     const [editing, setEditing] = useState(false);
     const initialFormState = { id: null, title: '', content: '' };
     const [currentPost, setCurrentPost] = useState(initialFormState);
 
-
-    const editRow = post => {
-        console.log("editRow, post is:");
-        console.log(post);
-        setEditing(true);
-        setCurrentPost({ id: post.id, title: post.title, content: post.content })
-    };
-
-    const {data, loading, error} = useQuery(GET_ALL_POSTS);
+    // apollo hooks
+    const {data: getAllPostsQuery, loading: getAllPostsLoading, error: getAllPostsError} = useQuery(GET_ALL_POSTS);
+    const {data: getPostFields, loading: getPostFieldsLoading, error: getPostFieldsError} = useQuery(GET_POST_FIELDS);
 
     const [addPost] = useMutation(CREATE_POST, {
         refetchQueries: [{ query: GET_ALL_POSTS }],
@@ -68,15 +80,33 @@ function App() {
         refetchQueries: [{ query: GET_ALL_POSTS }],
     });
 
+    // useEffect(() => alert("hey"))
 
 
-    if (loading) return (<p>Loading...</p>);
-    if (error) return (<p>Error: {JSON.stringify(error)}</p>);
+    // get post fields
+    // todo do something with this
+    if (getPostFieldsLoading) return (<p>Loading post fields...</p>);
+    if (getPostFieldsError) return (<p>Error getting post fields: {JSON.stringify(getAllPostsError)}</p>);
+    let fields = getPostFields["__type"].fields.map(field => { return {name: field.name, type: field.type.name} });
+    console.log("post fields:");
 
-    console.log(data);
-    const posts = data.allPosts;
+    console.log(fields);
 
 
+
+    // get all posts
+    if (getAllPostsLoading) return (<p>Loading posts...</p>);
+    if (getAllPostsError) return (<p>Error getting posts: {JSON.stringify(getAllPostsError)}</p>);
+    const posts = getAllPostsQuery.allPosts;
+    console.log("posts:");
+    console.log(posts);
+
+    const editRow = post => {
+        console.log("editRow, post is:");
+        console.log(post);
+        setEditing(true);
+        setCurrentPost({ id: post.id, title: post.title, content: post.content })
+    };
 
     return (
         <div className="container">
--- admin-app/src/forms/EditPostForm.js
+++ admin-app/src/forms/EditPostForm.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect  } from 'react'
 
 const EditPostForm = props => {
     const [currentPost, setCurrentPost] = useState(props.currentPost);
-    let id, title, content;
 
     const handleInputChange = event => {
         const { name, value } = event.target;
-
         setCurrentPost({ ...currentPost, [name]: value })
     };
 
     // In the Effect Hook, we create a callback function that updates the user state with the new prop thats being sent through. Before, we needed to compare if (prevProps.currentUser !== this.state.currentUser), but with the Effect Hook we can just pass [props] through to let it know we're watching props.
+    // The Effect Hook lets you perform side effects in function components:
+    //By using this Hook, you tell React that your component needs to do something after render. React will remember the function you passed (we’ll refer to it as our “effect”), and call it later after performing the DOM updates. In this effect, we set the document title, but we could also perform data fetching or call some other imperative API.
     useEffect(() => {
         setCurrentPost(props.currentPost)
     }, [props]);
@@ -18,22 +18,21 @@ const EditPostForm = props => {
     return (
         <form onSubmit={e => {
             e.preventDefault();
+            console.log(currentPost)
 
-            props.editPost({ variables: {
-                    id: id.value,
-                    authorId: "test",
-                    title: title.value,
-                    content: content.value,
-                } });
+            // todo debug
+            currentPost.authorId = "test";
+
+            props.editPost({ variables: currentPost });
 
         }}>
 
             <label>Title</label>
-            <input type="text" name="title" value={currentPost.title} onChange={handleInputChange} ref={node => {title = node;}} />
+            <input type="text" name="title" value={currentPost.title} onChange={handleInputChange} />
             <label>Content</label>
-            <input type="text" name="content" value={currentPost.content} onChange={handleInputChange} ref={node => {content = node;}} />
+            <input type="text" name="content" value={currentPost.content} onChange={handleInputChange} />
 
-            <input type="hidden" name="id" value={currentPost.id} onChange={handleInputChange} ref={node => {id = node;}} />
+            <input type="hidden" name="id" value={currentPost.id} onChange={handleInputChange} />
 
             <button>Update post</button>
             <button onClick={() => props.setEditing(false)} className="button muted-button">
