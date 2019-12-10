3a469d7589ec2e4ba76be36527617a5a712f1ade
Evgeni Leonti <evgeni.leonti@finanda.co.il>
2019-12-10 21:28:31 +0200

    dynamically get all fields of Post

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -7,135 +7,60 @@ import PostTable from './tables/PostTable'
 import AddPostForm from './forms/AddPostForm'
 import gql from "graphql-tag";
 import EditPostForm from "./forms/EditPostForm";
+import Crud from "./Crud";
 
 
-const GET_POST_FIELDS = gql`
+const GET_TYPES = gql`
     query {
-        __type(name: "Post") {
-            name
-            fields {
+        __schema {
+            types {
                 name
-                type {
+                fields {
                     name
+                    type {
+                        name
+                    }
                 }
             }
         }
     }`;
 
-
-const GET_ALL_POSTS = gql`
-    query {
-        allPosts {
-            title subtitle author {id name} category summary content id createdAt modifiedAt
-        }
-    }
-`;
-
-const CREATE_POST = gql`
-    mutation CreatePost($authorId: String!, $title: String!, $content: String!) {
-        createPost(authorId: $authorId, title: $title, content: $content) {
-            id title summary content createdAt
-        }
-    }
-`;
-
-const EDIT_POST = gql`
-    mutation EditPost($id: String!, $authorId: String!, $title: String!, $content: String!){
-        updatePost(id: $id, authorId: $authorId, title: $title, content: $content) {
-            id
-        }
-    }`;
-
-const DELETE_POST = gql`
-    mutation DeletePost($id: String!){
-        deletePost(id: $id) {id}
-    }
-`;
-
 function App() {
-    // Declare a new state variable named "editing". state variables are preserved by React.
-    // useState returns a pair of values: the current state and a function that updates it
-
-    // custom hooks
-    const [editing, setEditing] = useState(false);
-    const initialFormState = { id: null, title: '', content: '' };
-    const [currentPost, setCurrentPost] = useState(initialFormState);
-
-    // apollo hooks
-    const {data: getAllPostsQuery, loading: getAllPostsLoading, error: getAllPostsError} = useQuery(GET_ALL_POSTS);
-    const {data: getPostFields, loading: getPostFieldsLoading, error: getPostFieldsError} = useQuery(GET_POST_FIELDS);
-
-    const [addPost] = useMutation(CREATE_POST, {
-        refetchQueries: [{ query: GET_ALL_POSTS }],
-    });
-
-    const [editPost] = useMutation(EDIT_POST, {
-        refetchQueries: [{ query: GET_ALL_POSTS }],
-        update() {
-            setEditing(false);
-        }
-    });
-
-    const [deletePost] = useMutation(DELETE_POST, {
-        refetchQueries: [{ query: GET_ALL_POSTS }],
-    });
-
-    // useEffect(() => alert("hey"))
-
+    const {data, loading, error} = useQuery(GET_TYPES);
 
     // get post fields
     // todo do something with this
-    if (getPostFieldsLoading) return (<p>Loading post fields...</p>);
-    if (getPostFieldsError) return (<p>Error getting post fields: {JSON.stringify(getAllPostsError)}</p>);
-    let fields = getPostFields["__type"].fields.map(field => { return {name: field.name, type: field.type.name} });
-    console.log("post fields:");
+    if (loading) return (<p>Loading post fields...</p>);
+    if (error) return (<p>Error getting post fields: {JSON.stringify(error)}</p>);
 
-    console.log(fields);
+    let types = data["__schema"].types.filter(type => type.name.length > 1 && type.name.charAt(0) !== "_" && type.name.charAt(1) !== "_");
+    console.log("types:");
+    console.log(types);
 
+    let postFields = types.find(type => type.name === "Post").fields;
+    console.log("postFields:");
+    console.log(postFields);
 
+    let postFieldsStr = ``;
 
-    // get all posts
-    if (getAllPostsLoading) return (<p>Loading posts...</p>);
-    if (getAllPostsError) return (<p>Error getting posts: {JSON.stringify(getAllPostsError)}</p>);
-    const posts = getAllPostsQuery.allPosts;
-    console.log("posts:");
-    console.log(posts);
+    for (const field of postFields) {
+        postFieldsStr += field.name;
 
-    const editRow = post => {
-        console.log("editRow, post is:");
-        console.log(post);
-        setEditing(true);
-        setCurrentPost({ id: post.id, title: post.title, content: post.content })
-    };
+        let typeFields = types.find(type => type.name === field.type.name).fields;
+        if(typeFields) {
+            postFieldsStr += ` { ${typeFields.map(typeField => typeField.name).join(" ")} }`
+        }
+        postFieldsStr += ` `;
+    }
+
+    console.log(postFieldsStr);
+
 
     return (
-        <div className="container">
-            <h1>CRUD App with Hooks</h1>
-            <div className="flex-row">
-                <div className="flex-large">
-                    {editing ? (
-                        <div>
-                            <h2>Edit post</h2>
-                            <EditPostForm
-                                editing={editing}
-                                setEditing={setEditing}
-                                currentPost={currentPost}
-                                editPost={editPost}
-                            />
-                        </div>
-                    ) : (
-                        <div>
-                            <h2>Add post</h2>
-                            <AddPostForm addPost={addPost} />
-                        </div>
-                    )}
-                </div>
-                <div className="flex-large">
-                    <h2>View posts</h2>
-                    <PostTable posts={posts} editRow={editRow} deletePost={deletePost}/>
-                </div>
-            </div>
-        </div>
+        <React.Fragment>
+            <Crud postFieldsStr={postFieldsStr}>
+            </Crud>
+        </React.Fragment>
     );
 }
 
---
+++ admin-app/src/Crud.js
@@ -0,0 +1,116 @@
+import React, {useState} from "react";
+import EditPostForm from "./forms/EditPostForm";
+import AddPostForm from "./forms/AddPostForm";
+import PostTable from "./tables/PostTable";
+import {useMutation, useQuery} from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+function Crud(props) {
+    let fields = props.postFieldsStr;
+
+    const GET_ALL_POSTS = gql`
+        query {
+            allPosts {
+                ${fields}
+            }
+        }
+    `;
+
+    const CREATE_POST = gql`
+        mutation CreatePost($authorId: String!, $title: String!, $content: String!) {
+            createPost(authorId: $authorId, title: $title, content: $content) {
+                id title summary content createdAt
+            }
+        }
+    `;
+
+    const EDIT_POST = gql`
+        mutation EditPost($id: String!, $authorId: String!, $title: String!, $content: String!){
+            updatePost(id: $id, authorId: $authorId, title: $title, content: $content) {
+                id
+            }
+        }`;
+
+    const DELETE_POST = gql`
+        mutation DeletePost($id: String!){
+            deletePost(id: $id) {id}
+        }
+    `;
+
+    // Declare a new state variable named "editing". state variables are preserved by React.
+    // useState returns a pair of values: the current state and a function that updates it
+
+    // custom hooks
+    const [editing, setEditing] = useState(false);
+    const initialFormState = { id: null, title: '', content: '' };
+    const [currentPost, setCurrentPost] = useState(initialFormState);
+
+    // apollo hooks
+    const {data: getAllPostsQuery, loading: getAllPostsLoading, error: getAllPostsError} = useQuery(GET_ALL_POSTS);
+
+    const [addPost] = useMutation(CREATE_POST, {
+        refetchQueries: [{ query: GET_ALL_POSTS }],
+    });
+
+    const [editPost] = useMutation(EDIT_POST, {
+        refetchQueries: [{ query: GET_ALL_POSTS }],
+        update() {
+            setEditing(false);
+        }
+    });
+
+    const [deletePost] = useMutation(DELETE_POST, {
+        refetchQueries: [{ query: GET_ALL_POSTS }],
+    });
+
+
+
+
+
+    // get all posts
+    if (getAllPostsLoading) return (<p>Loading posts...</p>);
+    if (getAllPostsError) return (<p>Error getting posts: {JSON.stringify(getAllPostsError)}</p>);
+    const posts = getAllPostsQuery.allPosts;
+    // console.log("posts:");
+    // console.log(posts);
+
+    const editRow = post => {
+        console.log("editRow, post is:");
+        console.log(post);
+        setEditing(true);
+        setCurrentPost({ id: post.id, title: post.title, content: post.content })
+    };
+
+
+    return (
+        <div className="container">
+            <h1>CRUD App with Hooks</h1>
+            <div className="flex-row">
+                <div className="flex-large">
+                    {editing ? (
+                        <div>
+                            <h2>Edit post</h2>
+                            <EditPostForm
+                                editing={editing}
+                                setEditing={setEditing}
+                                currentPost={currentPost}
+                                editPost={editPost}
+                            />
+                        </div>
+                    ) : (
+                        <div>
+                            <h2>Add post</h2>
+                            <AddPostForm addPost={addPost} />
+                        </div>
+                    )}
+                </div>
+                <div className="flex-large">
+                    <h2>View posts</h2>
+                    <PostTable posts={posts} editRow={editRow} deletePost={deletePost}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Crud;
