fc43d0080e07a15e153b8e452085ee6e127bedfc
Jenya <leo@campus.technion.ac.il>
2019-12-16 15:08:14 +0200

    EditForm: display autoProps and updated modifiedAt value

--- admin-app/src/forms/EditForm.js
+++ admin-app/src/forms/EditForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import gql from "graphql-tag";
 import {useMutation} from "@apollo/react-hooks";
 
@@ -18,14 +18,30 @@ const EditForm = props => {
     const EDIT_MUTATION = gql`
         mutation Edit${entity.name}($id: String!, ${entity.mutationParams}){
             update${entity.name}(id: $id, ${entity.mutationVars}) {
-                id
+                ${entity.fields}
             }
         }`;
     const [createEntity, mutationResult] = useMutation(EDIT_MUTATION);
 
+
+    useEffect(() => {
+        if (mutationResult.called && !mutationResult.loading && !mutationResult.error) {
+            console.log("mutationResult");
+            console.log(mutationResult);
+
+            let newFields = mutationResult.data[`update${entity.name}`];
+            console.log("newFields");
+            console.log(newFields);
+            setCurrentEntity(newFields);
+        }
+     }
+    );
+
+
     if (mutationResult.loading) {
         return <div>Loading...</div>
     }
+
 
 
     let form =  <React.Fragment>
@@ -37,7 +53,31 @@ const EditForm = props => {
             createEntity({ variables: currentEntity })
 
         }}>
+            <div className="row">
+
+            {entity.autoProps.length > 0 ? (
+                entity.autoProps.map(arg => (
+                    <div className="col">
+                        <div key={arg.name} className="form-group">
+                            <label>{arg.name}</label>
+                            <input
+                             readOnly
+                             name={arg.name}
+                             type="text"
+                             className="form-control form-control-user"
+                             value={currentEntity[arg.name]}
+                             onChange={handleInputChange}
+                             // placeholder={arg.name}
+                            />
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <td>no fields</td>
+            )}
+            </div>
             {entity.manualProps.length > 0 ? (
+
                 entity.manualProps.map(arg => (
                     <div key={arg.name} className="form-group">
                         <label>{arg.name}</label>
@@ -62,6 +102,10 @@ const EditForm = props => {
 
     if (mutationResult.called) {
         if (!mutationResult.error) {
+            let newFields = mutationResult.data[`update${entity.name}`];
+            console.log("newFields");
+            console.log(newFields);
+            // setCurrentEntity(newFields);
             return (
                 <React.Fragment>
                     <div>Successfully updated. </div>
